ac9127c14da063523c4fac9a2669d689e1c5ee74
zygyy <1297799065@qq.com>
2019-06-20 15:23:04 +0800

    [Codes-update]:-商家主界面，静态资源引入，spring-mvc文件更改，添加静态资源映射驱动；mapper的方法未完善

---
+++ src/main/java/com/blm/service/StoreDetailService.java
@@ -0,0 +1,22 @@
+package com.blm.service;
+
+import com.blm.bean.StoreDetail;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 周宇
+ * @university ycit.com
+ * @creat 2019/6/20 11:21
+ */
+public interface StoreDetailService {
+    public List<StoreDetail> find(Map<String, Object> map);//查询所有店面
+
+
+    public Long getTotal(Map<String, Object> map);//查询总记录数
+
+
+    public int update(StoreDetail storeDetail);//更新店面信息
+}
+
