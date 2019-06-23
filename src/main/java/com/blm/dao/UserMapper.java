d86532b1cd998d013e721673494eafc6491881ce
“kris <373020024@qq.com>
2019-06-23 14:14:39 +0800

    [Code-Updates]:增加mybatis注解开发功能

--- src/main/java/com/blm/dao/UserMapper.java
+++ src/main/java/com/blm/dao/UserMapper.java
@@ -5,6 +5,27 @@ import com.blm.bean.User;
 import java.util.List;
 
 public interface UserMapper {
+//    注解开发
+//    int deleteByPrimaryKey(String userid);
+
+//    @Insert("insert into user(userid, username, password, phone, createtime, roleid, isvalid) values(#{userid},#{username},#{password},#{phone},#{createtime},#{roleid},#{isvalid})")
+//    int insert(User user);
+
+//    @Select("<script>select * from user <where><if test='username != null'>and username = #{username}</if>" +
+//            "<if test='username != null'>and username = #{username}</if>" +
+//            "<if test='username != null'>and username = #{username}</if>" +
+//            "<if test='username != null'>and username = #{username}</if>" +
+//            "<if test='username != null'>and username = #{username}</if>" +
+//            "<if test='username != null'>and username = #{username}</if>" +
+//            "<if test='username != null'>and username = #{username}</if></where></script>")
+//    List<User> selectSelective(User user);
+
+//    @Select("select * from User")
+//    public List<User> selectAllUser();
+
+//    @Select("select * from User where phone = #{phone}")
+//    public User selectUserByPhone(String phone);
+
     int deleteByPrimaryKey(String userid);
 
     int insert(User record);
